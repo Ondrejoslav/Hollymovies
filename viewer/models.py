a48866cb49684e7c4ca7f66890959883f5433d5c
ondrejoslav <ondrej.hak@email.cz>
2024-05-25 17:19:06 +0200

    Init. 3

--- viewer/models.py
+++ viewer/models.py
@@ -1,7 +1,30 @@
-from django.db.models import Model, CharField
+from django.db.models import Model, CharField, ForeignKey, DO_NOTHING, IntegerField, DateField, TextField, DateTimeField
 
 
 # Create your models here.
 
 class Genre(Model):
-    name = CharField(max_length=100)
+    name = CharField(max_length=100, null=False, blank=False)
+
+    def __repr__(self):
+        return f'<Genre: {self.name}>'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Movie(Model):
+    title = CharField(max_length=185, null=False, blank=False)
+    genre = ForeignKey(Genre, on_delete=DO_NOTHING)
+    rating = IntegerField(null=True, blank=True)
+    released = DateField()
+    description = TextField(null=True, blank=True)
+    created = DateTimeField(auto_now_add=True)
+
+
+    def __repr__(self):
+        return f'<Movie: {self.title}>'
+
+
+    def __str__(self):
+        return f'{self.title} ({self.released})'
